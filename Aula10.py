c8039f5a249fe07fa13a4b0df9045604c7ba2a6f
fernando <fernandocostagomes@gmail.com>
2018-10-16 22:38:59 -0300

    Video Aula 10

---
+++ Aula10.py
@@ -0,0 +1,36 @@
+'''
+Tipos Primitivos - INT, FLOAT, BOOL E STR
+Curso de Python
+Diego M. Rodrigues
+'''
+#numero1 = int(input('Número 1: '))
+#print(type(numero1))
+
+#numero2 = int(input('Número 2: '))
+#print(type(numero2))
+
+#soma = numero1 + numero2
+#print(type(soma))
+
+#print ('A soma de', numero1, 'e', numero2, 'vale', soma)
+#print('A soma de {} e {} vale {}'. format(numero1, numero2, soma))
+
+#numero3 = float(input('Número: '))
+#print(type(numero3))
+#print(numero3)
+
+#numero4 = bool(input('Número: '))
+#print(type(numero4))
+#print(numero4)
+
+#numero5 = True
+#numero6 = False
+#print(numero5, numero6)
+
+#numero7 = input('Nome: ')
+#print(type(numero7))
+#print(numero7)
+
+numero8 = input('Nome: ')
+print(type(numero8))
+print(numero8.isnumeric())
