fdedd9eb20127c46d9e22d2485f1d82bff4507bf
fernando <fernandocostagomes@gmail.com>
2018-10-16 22:18:06 -0300

    Primeiro exercicio

---
+++ AulaV4.py
@@ -0,0 +1,26 @@
+'''
+Exercício 1
+Um exercicio que leia o nome de uma pessoa, depois exiba na tela uma mensagem de
+boas vindas com o nome da pessoa digitado.
+'''
+nome=input('Qual seu nome por favor? ')
+print('Seja bem vinda ' + nome)
+
+'''
+Exercício 2
+Receba o dia, o mês e o ano de nascimento de uma pessoa.
+Exiba a data formatada.
+'''
+dia=input('Qual o dia do seu nascimento? ')
+mes=input('Qual o mês do seu nascimento? ')
+ano=input('Qual o ano do seu nascimento? ')
+print('Data de nascimento: ' + dia +'/'+mes+'/'+ano)
+
+'''
+Crie um script que leia 2 numeros e mostre a soma deles na tela
+'''
+numero1=input('Digite o primeiro número: ')
+numero2=input('Digite o segundo número: ')
+print('Resultado: ', int(numero1) + int(numero2))
+
+
